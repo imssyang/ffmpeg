7654f5995630c49110d53d4031daa3e295ffe06a
imssyang <imssyang@gmail.com>
2022-05-15 01:51:37 +0800

    add setup

---
+++ setup/env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export FFMPEG_DISABLE_ENV=yes
+eval "optbin -s /opt/ffmpeg/bin"
+eval "optman -s /opt/ffmpeg/share/man"
+eval "optpkg -s /opt/ffmpeg/lib/pkgconfig"
+eval "optlib -s /opt/ffmpeg/lib"
+
