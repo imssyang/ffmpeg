ce6fee3864f978f9221005dd78e6e8f016a27863
imssyang <imssyang@gmail.com>
2022-05-15 02:19:30 +0800

    add alias

--- setup/env.sh
+++ setup/env.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 export FFMPEG_DISABLE_ENV=yes
+
 eval "optbin -s /opt/ffmpeg/bin"
 eval "optman -s /opt/ffmpeg/share/man"
 eval "optpkg -s /opt/ffmpeg/lib/pkgconfig"
 eval "optlib -s /opt/ffmpeg/lib"
+
+alias ffmpeg="ffmpeg -hide_banner"
 
