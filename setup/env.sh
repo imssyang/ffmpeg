c6b5f6c0f00fdc8899279be3f17c4045477e3850
imssyang <imssyang@gmail.com>
2022-11-16 17:56:17 +0800

    add more help

--- setup/env.sh
+++ setup/env.sh
@@ -7,7 +7,7 @@ eval "optman -s /opt/ffmpeg/share/man"
 eval "optpkg -s /opt/ffmpeg/lib/pkgconfig"
 eval "optlib -s /opt/ffmpeg/lib"
 
-alias ffmpeg="ffmpeg -hide_banner"
-alias ffplay="ffplay -hide_banner"
-alias ffprobe="ffprobe -hide_banner"
+alias ffmpeg="ffmpeg -hide_banner -probesize 300000000"
+alias ffplay="ffplay -hide_banner -probesize 300000000 -x 1067 -y 600 -left 150 -top 150 -an -infbuf"
+alias ffprobe="ffprobe -hide_banner -probesize 300000000 -sexagesimal -pretty -unit"
 
