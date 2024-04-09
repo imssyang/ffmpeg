1663d73e6846c5c80660064df05de789481feada
imssyang <imssyang@gmail.com>
2024-04-09 17:42:31 +0800

    update env.sh

--- setup/env.sh
+++ setup/env.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-export FFMPEG_DISABLE_ENV=yes
-
-eval "optbin -s /opt/ffmpeg/bin"
-eval "optman -s /opt/ffmpeg/share/man"
-eval "optpkg -s /opt/ffmpeg/lib/pkgconfig"
-eval "optlib -s /opt/ffmpeg/lib"
-
 alias ffmpeg="ffmpeg -hide_banner -probesize 300000000"
 alias ffplay="ffplay -hide_banner -probesize 300000000 -x 1067 -y 600 -left 150 -top 150 -an -infbuf"
 alias ffprobe="ffprobe -hide_banner -probesize 300000000 -sexagesimal -pretty -unit"
