223c6ba88a505f379684587cf5ba3af50ebd8754
imssyang <imssyang@gmail.com>
2024-08-01 23:06:58 +0800

    enable audio for ffplay

--- setup/env.sh
+++ setup/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 alias ffmpeg="ffmpeg -hide_banner -probesize 300000000"
-alias ffplay="ffplay -hide_banner -probesize 300000000 -x 1067 -y 600 -left 150 -top 150 -an -infbuf"
+alias ffplay="ffplay -hide_banner -probesize 300000000 -x 1067 -y 600 -left 150 -top 150 -infbuf"
 alias ffprobe="ffprobe -hide_banner -probesize 300000000 -sexagesimal -pretty -unit"
 
